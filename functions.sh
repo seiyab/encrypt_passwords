fc143c4c7f8e9fc2a5b3520d957e2642d4172489
seiya <hoge@hoge>
2017-07-08 15:45:54 +0900

    first commit

---
+++ functions.sh
@@ -0,0 +1,30 @@
+function encpass() {
+    if [ -z "$2" ]; then
+        echo "Short of argument"
+    elif [ -f $MY_PASSWORDS"/"$1".txt.enc" ]; then
+        echo "Password already exists."
+    else
+        echo $2 | openssl enc -aes-256-cbc -base64 -md sha1 -out $MY_PASSWORDS"/"$1".txt.enc"
+    fi
+}
+
+function genpass() {
+    if [ -z "$1" ]; then
+        echo "Short of argument"
+    elif [ -f $MY_PASSWORDS"/"$1".txt.enc" ]; then
+        echo "Password already exists."
+    else
+        openssl rand -base64 12 | openssl enc -aes-256-cbc -base64 -md sha1 -out $MY_PASSWORDS"/"$1".txt.enc" \
+     && chmod 400 $MY_PASSWORDS"/"$1".txt.enc"
+    fi
+}
+
+function decpass() {
+    if [ -z "$1" ]; then
+        echo "Short of argument"
+    elif [ -f $MY_PASSWORDS"/"$1".txt.enc" ]; then
+        openssl enc -d -aes-256-cbc -base64 -md sha1 -in $MY_PASSWORDS"/"$1".txt.enc"
+    else
+        echo "Password doesn't exist."
+    fi
+}
